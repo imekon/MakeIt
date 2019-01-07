a1d92825417754381745e5d770663092f452ede1
Pete Goodwin <gitkraken@imekon.org>
2019-01-07 19:37:21 +0000

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,42 @@
+#include <imgui.h>
+#include <imgui-SFML.h>
+
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System/Clock.hpp>
+#include <SFML/Window/Event.hpp>
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
+	window.setFramerateLimit(60);
+	ImGui::SFML::Init(window);
+
+	sf::Clock deltaClock;
+	while (window.isOpen()) 
+	{
+		sf::Event event;
+		while (window.pollEvent(event)) 
+		{
+			ImGui::SFML::ProcessEvent(event);
+
+			if (event.type == sf::Event::Closed) 
+			{
+				window.close();
+			}
+		}
+
+		ImGui::SFML::Update(window, deltaClock.restart());
+
+		ImGui::Begin("Hello, world!");
+		ImGui::Button("Look at this pretty button");
+		ImGui::End();
+
+		window.clear();
+		ImGui::SFML::Render(window);
+		window.display();
+	}
+
+	ImGui::SFML::Shutdown();
+
+	return 0;
+}
